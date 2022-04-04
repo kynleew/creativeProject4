3fe2217695c70c33a5030ed1d918aa0aa9828ae6
kynleew <kynlee.wil@gmail.com>
2022-04-04 17:47:01 -0600

    server

--- back-end/server.js
+++ back-end/server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 // Configure multer so that it will upload to '../front-end/public/images'
 const multer = require('multer')
 const upload = multer({
-  dest: '/var/www/lab4.kynleew.live',
+  dest: 'test.kynleew.live',
   limits: {
     fileSize: 10000000
   }
